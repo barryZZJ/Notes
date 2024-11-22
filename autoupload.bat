a05abd6c1cf5d444f1214995200ab929ebe1c5d6
barryZZJ <850462618@qq.com>
2024-11-22 16:13:49 +0800

    2024/11/22_16:13:49

--- autoupload.bat
+++ autoupload.bat
@@ -1,4 +1,13 @@
-
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+	echo Running with admin privileges
+) ELSE (
+	echo Running without admin privileges
+	echo Requesting admin privileges...
+	powershell Start-Process -Verb RunAs -FilePath "%0"
+	exit /B
+)
+cd /d %~dp0
 git add .
 git commit -m "%date:~0,10%_%time:~0,8%"
 git pull
